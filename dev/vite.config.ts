90bca0225be69a0f182e5ea9d4925a0c43259865
bigmistqke <vandijckv@gmail.com>
2024-04-30 03:27:06 +0200

    add base to vite.config

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -4,6 +4,7 @@ import solidPlugin from 'vite-plugin-solid'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  base: './',
   plugins: [
     tsconfigPaths(),
     solidPlugin({
