4b0d832b28388f7c18bd15e32441bea714c45156
bigmistqke <vandijckv@gmail.com>
2024-05-08 21:09:53 +0200

    shiki-editor: refactor lineSize to signal and pass less html
    before
    - lineSize was a memo that updated when source updated
    - when copy+pasting and overflowing it would not scroll as expected, but instead scroll to 0px, because the textarea's value would wrap before its size was updated
    
    after
    - lineSize is a signal
        - updated when file changes with createRenderEffect and untrack
        - updated empirically inside onInput-handler of textarea
    - this way the textarea's size would always be up-to-date and no unwanted wrapping would occur

--- src/components/editors/shiki-editor.tsx
+++ src/components/editors/shiki-editor.tsx
@@ -11,12 +11,12 @@ import {
   Show,
   Suspense,
   createMemo,
+  createRenderEffect,
   createResource,
   createSignal,
   onCleanup,
-  splitProps,
+  untrack,
   useTransition,
-  type ComponentProps,
   type JSX,
 } from 'solid-js'
 import { Dynamic } from 'solid-js/web'
@@ -28,19 +28,15 @@ import styles from './shiki-editor.module.css'
 type Root = Awaited<ReturnType<typeof codeToHast>>
 type HastNode = Root['children'][number]
 
-export function ReplShikiEditor(
-  props: Omit<ComponentProps<'div'>, 'style' | 'theme'> & {
-    editorStyle?: JSX.CSSProperties
-    editorClass?: string
-    style?: JSX.CSSProperties
-    path: string
-    themes?: {
-      light?: CodeOptionsSingleTheme<BundledTheme>['theme']
-      dark?: CodeOptionsSingleTheme<BundledTheme>['theme']
-    }
-  },
-) {
-  const [, rest] = splitProps(props, ['style', 'themes', 'editorStyle', 'editorClass'])
+export function ReplShikiEditor(props: {
+  class?: string
+  path: string
+  style?: JSX.CSSProperties
+  themes?: {
+    light?: CodeOptionsSingleTheme<BundledTheme>['theme']
+    dark?: CodeOptionsSingleTheme<BundledTheme>['theme']
+  }
+}) {
   const startTransition = useTransition()[1]
   const runtime = useRuntime()
 
@@ -58,29 +54,26 @@ export function ReplShikiEditor(
   // Transform source to hast (hypertext abstract syntax tree)
   const [hast] = createResource(
     source,
-    source => codeToHast(source, { lang: 'tsx', theme: currentTheme() }),
+    async source => (await codeToHast(source, { lang: 'tsx', theme: currentTheme() })).children[0],
     { storage: createDeepSignal },
   )
 
-  // Signal referring to the width of a single character
   const [characterWidth, setCharacterWidth] = createSignal<number>(0)
+  const [lineSize, setLineSize] = createSignal(0)
 
-  // Get the longest line-size whenever source changes
-  const lineSize = createMemo(
-    whenever(
-      source,
-      source => {
-        let lineSize = -Infinity
-        source.split('\n').forEach(line => {
-          if (line.length > lineSize) {
-            lineSize = line.length
-          }
-        })
-        return lineSize
-      },
-      () => 0,
-    ),
-  )
+  // Get the longest line-size of a given string
+  const updateLineSize = (source: string) => {
+    let lineSize = -Infinity
+    source.split('\n').forEach(line => {
+      if (line.length > lineSize) {
+        lineSize = line.length
+      }
+    })
+    setLineSize(lineSize)
+  }
+
+  // Whenever file changes get the source a single time and update the line-size
+  createRenderEffect(whenever(file, file => updateLineSize(untrack(file.get.bind(file)))))
 
   // Get styles from current theme
   const [themeStyles] = createResource(
@@ -98,30 +91,32 @@ export function ReplShikiEditor(
         }),
   )
 
-  const updateSource = (value: string) => startTransition(() => file().set(value))
+  const onUpdate = (e: { currentTarget: HTMLTextAreaElement }) => {
+    const value = e.currentTarget.value
+    // Update line-size with the maximum linesize of the given string
+    updateLineSize(value)
+    // Update source with startTransition so Suspense is not triggered.
+    startTransition(() => file().set(value))
+  }
 
   return (
-    <div
-      class={clsx(styles['outer-container'], props.class)}
-      style={{ ...themeStyles(), ...props.style }}
-      {...rest}
-    >
-      <div class={styles['inner-container']}>
-        <Suspense>
-          <div class={clsx(styles.output, props.editorClass)} style={props.editorStyle}>
-            <Index each={hast()?.children}>{child => <HastNode node={child()} />}</Index>
-          </div>
-        </Suspense>
-        <textarea
-          class={clsx(styles.input, props.editorClass)}
-          onInput={e => updateSource(e.currentTarget.value)}
-          spellcheck={false}
-          style={{ ...props.editorStyle, 'min-width': lineSize() * characterWidth() + 'px' }}
-          value={file().get()}
-        />
-        <Character onResize={setCharacterWidth} />
+    <Suspense>
+      <div class={clsx(styles.editor, props.class)} style={{ ...themeStyles(), ...props.style }}>
+        <div class={styles.container}>
+          <Show when={hast()?.children}>
+            {children => <Index each={children()}>{child => <HastNode node={child()} />}</Index>}
+          </Show>
+          <textarea
+            class={styles.input}
+            onInput={onUpdate}
+            spellcheck={false}
+            style={{ width: lineSize() * characterWidth() + 'px' }}
+            value={file().get()}
+          />
+          <Character onResize={setCharacterWidth} />
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
 
