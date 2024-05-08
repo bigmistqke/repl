fdabce44f36326de679327e0f8743e99d1a89b12
bigmistqke <vandijckv@gmail.com>
2024-05-08 18:48:35 +0200

    conditionals: update when/whenever with overloads
    before
    - when/wherever return TResult | undefined
    
    after
    -  fallback is not defined: when/wherever returns TResult | undefined
    - fallback is defined: when/wherever returns returns TResult | TFallbackResult

--- src/utils/conditionals.ts
+++ src/utils/conditionals.ts
@@ -1,22 +1,43 @@
 import { Accessor, Resource } from 'solid-js'
 
-/**
- * Executes a callback with a value derived from an accessor if the value is truthy.
- *
- * @param accessor The value or function returning a value that is checked for truthiness.
- * @param callback The callback function to be executed if the accessor's value is truthy.
- * @returns The result of the callback if executed, otherwise undefined.
- */
 export function when<
-  const T,
-  TAccessor extends Accessor<T> | T,
+  T,
+  const TAccessor extends Accessor<T> | T,
   const TValues extends TAccessor extends ((...args: any[]) => any) | undefined
     ? Exclude<ReturnType<Exclude<TAccessor, undefined>>, null | undefined | false>
     : Exclude<TAccessor, null | undefined | false>,
-  TResult,
->(accessor: TAccessor, callback: (value: TValues) => TResult, fallback?: () => TResult) {
+  const TResult,
+>(accessor: TAccessor, callback: (value: TValues) => TResult): TResult | undefined
+
+export function when<
+  T,
+  const TAccessor extends Accessor<T> | T,
+  const TValues extends TAccessor extends ((...args: any[]) => any) | undefined
+    ? Exclude<ReturnType<Exclude<TAccessor, undefined>>, null | undefined | false>
+    : Exclude<TAccessor, null | undefined | false>,
+  const TResult,
+  const TFallbackResult,
+>(
+  accessor: TAccessor,
+  callback: (value: TValues) => TResult,
+  fallback: () => TFallbackResult,
+): TResult | TFallbackResult
+
+export function when<
+  T,
+  const TAccessor extends Accessor<T> | T,
+  const TValues extends TAccessor extends ((...args: any[]) => any) | undefined
+    ? Exclude<ReturnType<Exclude<TAccessor, undefined>>, null | undefined | false>
+    : Exclude<TAccessor, null | undefined | false>,
+  const TResult,
+  const TFallbackResult,
+>(
+  accessor: TAccessor,
+  callback: (value: TValues) => TResult,
+  fallback?: () => TFallbackResult,
+): TResult | TFallbackResult | undefined {
   const value = typeof accessor === 'function' ? accessor() : accessor
-  return value ? callback(value) : fallback?.()
+  return value ? callback(value) : fallback ? fallback() : undefined
 }
 
 /**
@@ -27,13 +48,41 @@ export function when<
  * @returns A function that can be called to execute the callback conditionally based on the accessor's value.
  */
 export function whenever<
-  const T,
-  TAccessor extends Accessor<T> | T,
+  T,
+  const TAccessor extends Accessor<T> | T,
   const TValues extends TAccessor extends ((...args: any[]) => any) | undefined
     ? Exclude<ReturnType<Exclude<TAccessor, undefined>>, null | undefined | false>
     : Exclude<TAccessor, null | undefined | false>,
-  TResult,
->(accessor: TAccessor, callback: (value: TValues) => TResult, fallback?: () => TResult) {
+  const TResult,
+>(accessor: TAccessor, callback: (value: TValues) => TResult): () => TResult | undefined
+
+export function whenever<
+  const T,
+  const TAccessor extends Accessor<T> | T,
+  const TValues extends TAccessor extends ((...args: any[]) => any) | undefined
+    ? Exclude<ReturnType<Exclude<TAccessor, undefined>>, null | undefined | false>
+    : Exclude<TAccessor, null | undefined | false>,
+  const TResult,
+  const TFallbackResult,
+>(
+  accessor: TAccessor,
+  callback: (value: TValues) => TResult,
+  fallback: () => TFallbackResult,
+): () => TResult | TFallbackResult
+
+export function whenever<
+  T,
+  const TAccessor extends Accessor<T> | T,
+  const TValues extends TAccessor extends ((...args: any[]) => any) | undefined
+    ? Exclude<ReturnType<Exclude<TAccessor, undefined>>, null | undefined | false>
+    : Exclude<TAccessor, null | undefined | false>,
+  const TResult,
+  const TFallbackResult,
+>(
+  accessor: TAccessor,
+  callback: (value: TValues) => TResult,
+  fallback?: () => TFallbackResult,
+): () => TResult | TFallbackResult | undefined {
   return () => when(accessor, callback, fallback)
 }
 
