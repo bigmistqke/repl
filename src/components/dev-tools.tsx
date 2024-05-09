d3fd4a13790dbace219232dce7c2d404b2401e86
bigmistqke <vandijckv@gmail.com>
2024-05-09 16:02:37 +0200

    Add default path-prop to Repl.Frame

--- src/components/dev-tools.tsx
+++ src/components/dev-tools.tsx
@@ -1,5 +1,12 @@
 import clsx from 'clsx'
-import { ComponentProps, createEffect, createResource, onCleanup, splitProps } from 'solid-js'
+import {
+  ComponentProps,
+  createEffect,
+  createResource,
+  mergeProps,
+  onCleanup,
+  splitProps,
+} from 'solid-js'
 import { Frame } from 'src/runtime'
 import { whenever } from 'src/utils/conditionals'
 import { html, javascript } from 'src/utils/object-url-literal'
@@ -8,13 +15,14 @@ import { useRuntime } from '../use-runtime'
 // @ts-expect-error
 import styles from './repl.module.css'
 
-export type DevToolsProps = ComponentProps<'iframe'> & { name: string }
+export type DevToolsProps = ComponentProps<'iframe'> & { name?: string }
 
 export function ReplDevTools(props: DevToolsProps) {
-  const [, rest] = splitProps(props, ['class'])
+  const config = mergeProps({ name: 'default' }, props)
+  const [, rest] = splitProps(config, ['class'])
   const runtime = useRuntime()
 
-  const moduleUrl = html`<!doctype html>
+  const chiiModule = html`<!doctype html>
     <html lang="en">
       <meta charset="utf-8" />
       <title>DevTools</title>
@@ -34,16 +42,64 @@ export function ReplDevTools(props: DevToolsProps) {
       <body class="undocked" id="-blink-dev-tools"></body>
     </html>`
 
+  const chobitsuModule = javascript`
+    import('https://cdn.jsdelivr.net/npm/chobitsu').then(shobitsu => {
+      const sendToDevtools = (message) => {
+        window.parent.postMessage(JSON.stringify(message), '*');
+      };
+      let id = 0;
+      const sendToChobitsu = (message) => {
+        message.id = 'tmp' + ++id;
+        chobitsu.sendRawMessage(JSON.stringify(message));
+      };
+      chobitsu.setOnMessage((message) => {
+        if (message.includes('"id":"tmp')) return;
+        window.parent.postMessage(message, '*');
+      });
+      window.addEventListener('message', ({ data }) => {
+        try {
+          const { event, value } = data;
+          if (event === 'DEV') {
+            chobitsu.sendRawMessage(data.data);
+          } else if (event === 'LOADED') {
+            sendToDevtools({
+              method: 'Page.frameNavigated',
+              params: {
+                frame: {
+                  id: '1',
+                  mimeType: 'text/html',
+                  securityOrigin: location.origin,
+                  url: location.href,
+                },
+                type: 'Navigation',
+              },
+            });
+            sendToChobitsu({ method: 'Network.enable' });
+            sendToDevtools({ method: 'Runtime.executionContextsCleared' });
+            sendToChobitsu({ method: 'Runtime.enable' });
+            sendToChobitsu({ method: 'Debugger.enable' });
+            sendToChobitsu({ method: 'DOMStorage.enable' });
+            sendToChobitsu({ method: 'DOM.enable' });
+            sendToChobitsu({ method: 'CSS.enable' });
+            sendToChobitsu({ method: 'Overlay.enable' });
+            sendToDevtools({ method: 'DOM.documentUpdated' });
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      });
+    })`
+
   const iframe = (
     <iframe
-      src={`${moduleUrl}#?embedded=${encodeURIComponent(location.origin)}`}
-      class={clsx(props.class, styles.frame)}
+      src={`${chiiModule}#?embedded=${encodeURIComponent(location.origin)}`}
+      class={clsx(config.class, styles.frame)}
       {...rest}
     />
   ) as HTMLIFrameElement
 
   const [targetFrame] = createResource(
-    () => runtime.frameRegistry.get(props.name),
+    () => runtime.frameRegistry.get(config.name),
     frame => {
       if (frame.contentWindow.document.readyState === 'interactive') return frame
       return new Promise<Frame>(resolve => {
@@ -71,57 +127,7 @@ export function ReplDevTools(props: DevToolsProps) {
     }),
   )
 
-  createEffect(
-    whenever(targetFrame, targetFrame =>
-      targetFrame.injectModuleUrl(javascript`
-        import('https://cdn.jsdelivr.net/npm/chobitsu').then(shobitsu => {
-          const sendToDevtools = (message) => {
-            window.parent.postMessage(JSON.stringify(message), '*');
-          };
-          let id = 0;
-          const sendToChobitsu = (message) => {
-            message.id = 'tmp' + ++id;
-            chobitsu.sendRawMessage(JSON.stringify(message));
-          };
-          chobitsu.setOnMessage((message) => {
-            if (message.includes('"id":"tmp')) return;
-            window.parent.postMessage(message, '*');
-          });
-          window.addEventListener('message', ({ data }) => {
-            try {
-              const { event, value } = data;
-              if (event === 'DEV') {
-                chobitsu.sendRawMessage(data.data);
-              } else if (event === 'LOADED') {
-                sendToDevtools({
-                  method: 'Page.frameNavigated',
-                  params: {
-                    frame: {
-                      id: '1',
-                      mimeType: 'text/html',
-                      securityOrigin: location.origin,
-                      url: location.href,
-                    },
-                    type: 'Navigation',
-                  },
-                });
-                sendToChobitsu({ method: 'Network.enable' });
-                sendToDevtools({ method: 'Runtime.executionContextsCleared' });
-                sendToChobitsu({ method: 'Runtime.enable' });
-                sendToChobitsu({ method: 'Debugger.enable' });
-                sendToChobitsu({ method: 'DOMStorage.enable' });
-                sendToChobitsu({ method: 'DOM.enable' });
-                sendToChobitsu({ method: 'CSS.enable' });
-                sendToChobitsu({ method: 'Overlay.enable' });
-                sendToDevtools({ method: 'DOM.documentUpdated' });
-              }
-            } catch (e) {
-              console.error(e);
-            }
-          });
-        })`),
-    ),
-  )
+  createEffect(whenever(targetFrame, targetFrame => targetFrame.injectModuleUrl(chobitsuModule)))
 
   return iframe
 }
