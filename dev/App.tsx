8a9d43e0edbcfe8e75e58931ec10abbb1bec7a36
bigmistqke <vandijckv@gmail.com>
2024-08-02 15:29:44 +0200

    (fix) path-alias to @bigmistqke/repl/editor/monaco/themes

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,67 +1,38 @@
 import { DevTools, Frame, Repl, TabBar, useRuntime } from '@bigmistqke/repl'
 import { MonacoEditor, MonacoProvider, MonacoTheme } from '@bigmistqke/repl/editor/monaco'
-import { JsFile } from '@bigmistqke/repl/runtime'
+import vs_dark from '@bigmistqke/repl/editor/monaco/themes/vs_dark_good.json'
+import { JsFile, Runtime } from '@bigmistqke/repl/runtime'
 import { typescriptTransformModulePaths } from '@bigmistqke/repl/transform-module-paths/typescript'
 import { babelTransform } from '@bigmistqke/repl/transform/babel'
-import { Resizable } from 'corvu/resizable'
 import { createEffect, createSignal, mapArray, onCleanup, type Component } from 'solid-js'
-import vs_dark from 'src/editor/monaco/themes/vs_dark_good.json'
 import { babelSolidReplPlugin } from 'src/plugins/babel-solid-repl'
-import styles from './App.module.css'
 
 const tsconfig = {
-  target: /* ScriptTarget.ES2015  */ 2, // Output ES6 compatible code
-  module: /* ModuleKind.ES2015 */ 5, // Use ES6 modules
-  jsx: /* JsxEmit.Preserve */ 1, // Preserve JSX syntax for further processing with Babel
-  jsxImportSource: 'solid-js', // Use solid-js for JSX
-  esModuleInterop: true, // Enables emit interoperability between CommonJS and ES Modules
-  allowSyntheticDefaultImports: true, // Allow default imports from modules with no default export
-  forceConsistentCasingInFileNames: true, // Ensure consistent casing in file names
-  isolatedModules: true, // Ensure each file is treated as a separate module
-  resolveJsonModule: true, // Include JSON modules in TypeScript files
-  skipLibCheck: true, // Skip type checking of declaration files
-  strict: true, // Enable all strict type-checking options
-  noEmit: false, // Allow TypeScript to emit output files
-  outDir: './dist', // Specify output directory for compiled files
+  target: 2, // ScriptTarget.ES2015
+  module: 5, // ModuleKind.ES2015
+  jsx: 1, // JsxEmit.Preserve
+  jsxImportSource: 'solid-js',
+  esModuleInterop: true,
+  allowSyntheticDefaultImports: true,
+  forceConsistentCasingInFileNames: true,
+  isolatedModules: true,
+  resolveJsonModule: true,
+  skipLibCheck: true,
+  strict: true,
+  noEmit: false,
 }
 
 const Frames = () => {
-  const [isDragging, setIsDragging] = createSignal(false)
   return (
-    <Resizable.Panel
-      as={Resizable}
-      style={{ display: 'flex', overflow: 'hidden', flex: 1 }}
-      orientation="vertical"
-    >
-      <Resizable.Panel
-        as={Frame}
-        style={{
-          'min-height': 0,
-          'pointer-events': isDragging() ? 'none' : undefined,
-          display: 'flex',
-          overflow: 'none',
-        }}
+    <>
+      <Frame
         bodyStyle={{
           padding: '0px',
           margin: '0px',
         }}
       />
-      <Resizable.Handle
-        onDragStart={() => setIsDragging(true)}
-        onDragEnd={() => setIsDragging(false)}
-        class={styles.handle}
-      />
-      <Resizable.Panel
-        as={DevTools}
-        minSize={0}
-        name={'default'}
-        style={{
-          'min-height': 0,
-          'pointer-events': isDragging() ? 'none' : undefined,
-          overflow: 'hidden',
-        }}
-      />
-    </Resizable.Panel>
+      <DevTools name={'default'} />
+    </>
   )
 }
 
@@ -87,6 +58,30 @@ const App: Component = () => {
         add file
       </button>
     )
+  }
+
+  function onSetup({ fileSystem, frameRegistry }: Runtime) {
+    createEffect(() => {
+      const frame = frameRegistry.get('default')
+      if (!frame) return
+
+      const entry = fileSystem.get('src/index.tsx')
+
+      if (entry instanceof JsFile) {
+        createEffect(() => {
+          // inject entry's module-url into frame's window
+          frame.injectFile(entry)
+          onCleanup(() => frame.dispose(entry))
+        })
+
+        createEffect(
+          mapArray(entry.module.cssImports, css => {
+            createEffect(() => frame.injectFile(css))
+            onCleanup(() => frame.dispose(css))
+          }),
+        )
+      }
+    })
   }
 
   return (
@@ -129,54 +124,18 @@ render(() => <Counter />, document.body);
           },
         },
       }}
-      class={styles.repl}
-      onSetup={async ({ fileSystem, frameRegistry }) => {
-        createEffect(() => {
-          const frame = frameRegistry.get('default')
-          if (!frame) return
-
-          const entry = fileSystem.get('src/index.tsx')
-
-          if (entry instanceof JsFile) {
-            createEffect(() => {
-              // inject entry's module-url into frame's window
-              frame.injectFile(entry)
-              onCleanup(() => frame.dispose(entry))
-            })
-
-            createEffect(
-              mapArray(entry.module.cssImports, css => {
-                createEffect(() => frame.injectFile(css))
-                onCleanup(() => frame.dispose(css))
-              }),
-            )
-          }
-        })
-      }}
+      onSetup={onSetup}
     >
-      <Resizable style={{ width: '100vw', height: '100vh', display: 'flex' }}>
-        <Resizable.Panel
-          style={{ overflow: 'hidden', display: 'flex', 'flex-direction': 'column' }}
-        >
-          <div style={{ display: 'flex' }}>
-            <TabBar style={{ flex: 1 }}>
-              {({ path }) => <button onClick={() => setCurrentFile(path)}>{path}</button>}
-            </TabBar>
-            <AddButton />
-          </div>
-          {/* <ShikiEditor
-            theme={darkPlus}
-            lang={tsx}
-            style={{ flex: 1, padding: '20px' }}
-            path={currentPath()}
-          /> */}
-          <MonacoProvider theme={vs_dark as MonacoTheme} tsconfig={tsconfig}>
-            <MonacoEditor style={{ flex: 1 }} path={currentPath()} />
-          </MonacoProvider>
-        </Resizable.Panel>
-        <Resizable.Handle class={styles.handle} />
-        <Frames />
-      </Resizable>
+      <div>
+        <TabBar>
+          {({ path }) => <button onClick={() => setCurrentFile(path)}>{path}</button>}
+        </TabBar>
+        <AddButton />
+      </div>
+      <MonacoProvider theme={vs_dark as MonacoTheme} tsconfig={tsconfig}>
+        <MonacoEditor path={currentPath()} />
+      </MonacoProvider>
+      <Frames />
     </Repl>
   )
 }
