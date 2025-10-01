c835c4f76e41713ce12622d1127cf40363ecd8ae
bigmistqke <vandijckv@gmail.com>
2025-10-01 16:05:04 +0200

    chore: remove redundant demo

--- demo/demo/index.ts
+++
@@ -1,9 +0,0 @@
-import indexCss from './src/index.css?raw'
-import indexHtml from './src/index.html?raw'
-import mainTs from './src/main.ts?raw'
-
-export default {
-  'index.css': indexCss,
-  'index.html': indexHtml,
-  'main.ts': mainTs,
-}
